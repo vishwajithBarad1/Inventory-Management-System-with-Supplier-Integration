9ea05a60a7352d800b070c92fe661a24d9acb5d6
vishwajithBarad1 <vishwajithbarad@gmail.com>
2024-08-23 02:59:19 +0530

    created OrderDetails component to handle with rendering the orders with a cancle button

---
+++ frontend/src/components/OrderDetails.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import "../styles/ProductDetails.css"
+import axios from "axios"
+function OrderDetails({orderId, product_id, quantity, order_date, status, getAllOrders, header, orderPage}){
+    const [showDetail, setShowDetail] = useState(false);
+
+    function handleBackgroundColor(){
+        if (status === "Cancelled"){
+            return "rgba(255,0,0,0.5)"
+        }else{
+            return "rgba(0,255,0,0.5)"
+        }
+    }
+    function handleMouseEnter(){
+        setShowDetail(true);
+        console.log("MouseEntered")
+
+    }
+    function handleMouseLeave(){
+        setShowDetail(false);
+        console.log("MouseLeft")
+    }
+    async function handleCancel(){
+        try{
+            await axios.put(`http://localhost:4000/order/setCancel?id=${orderId}`,{},
+                {
+                    headers: {
+                      Authorization: localStorage.getItem("authToken")
+                    }
+                  }
+            );
+            getAllOrders();
+        }catch(error){
+            alert(error.message);
+            console.log(error);
+        }
+    }
+    return(
+        <div className="values_container" onMouseLeave={handleMouseLeave}> 
+            {showDetail?
+            <div>
+                <div className="product_values" style={{paddingTop:"25px",width:"208px"}}>{product_id.name}</div>
+                <div style={{
+                    position:"relative",
+                    backgroundColor: "rgba(255,255,255)",
+                    border: '1px solid #ccc',
+                    boxShadow: '0 0 10px rgba(0,0,0,0.5)',
+                    width: '200px',
+                    padding:"10px",
+                    left:"170px",
+                    marginBottom: '-300px',}}
+                    >
+                        <strong>{product_id.name}</strong>
+                        <p>SKU: {product_id.sku}</p>
+                        <p>{product_id.description}</p>
+                        <p>Price: ${product_id.price}</p>
+                        <p>Current_stock: {product_id.current_stock}</p>
+                </div>
+            </div>
+            :
+            <div className="product_values" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>{product_id.name}</div>}
+            <div className="product_values">{quantity}</div>
+            <div className="product_values">{order_date==="Date"?"Date":String(order_date).split("T")[0]}</div>
+            <div className="product_values" >{status}</div>
+            {status==="Pending"?
+            <div className="product_values" ><button className="delete" onClick={handleCancel}>Cancel Order</button></div>
+            :
+            <div className="product_values" style={{backgroundColor:handleBackgroundColor()}}></div>
+            }
+        </div>
+    )
+}
+
+export default OrderDetails;
