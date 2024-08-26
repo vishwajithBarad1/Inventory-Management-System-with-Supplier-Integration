95cf29ea4dd0b1ce541847d7311d66c9ab8711b7
vishwajithBarad1 <vishwajithbarad@gmail.com>
2024-08-26 22:49:17 +0530

    added a dropdown to select the product insted of enterning the product id

--- frontend/src/components/AddOrder.jsx
+++ frontend/src/components/AddOrder.jsx
@@ -1,11 +1,12 @@
 //product_id, quantity, order_date
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../styles/AddProduct.css"
 import axios from "axios";
 function AddOrder({getAllOrders}){
     const [product_id,setProduct_id] = useState(null);
     const [quantity,setQuantity] = useState(null);
     const [order_date,setOrder_date] = useState(Date.now());
+    const [products,setProducts] = useState([]);
 
     async function createNewOrder(){
         try{
@@ -34,14 +35,35 @@ function AddOrder({getAllOrders}){
         }
     }
     
+    async function getAllProducts(){
+        try{
+            const response = await axios.get("http://localhost:4000/product/getAllProducts",{
+                headers:{
+                    'Authorization':localStorage.getItem("authToken")
+                }
+            });
+            setProducts(response.data.data);
+        }catch(e){
+            console.error("Error fetching products",e);
+            alert("Error fetching products")
+        }
+    }
+    function handleChange(event){
+        setProduct_id(event.target.value);
+        console.log({product_id});
+    }
+    useEffect(()=>{
+        getAllProducts();
+    },[]);
+    
     return(
         <>
             <div className="inputBar">
-                <input type="text" placeholder="Product Id" value={product_id} onChange={(event)=>{setProduct_id(event.target.value);}}/>
+                <select className="selectProduct" onChange={handleChange}><option value="ProductName">Product Name</option>{products.map((product)=>{return <option value={product._id} > {product.name}</option>})}</select>
                 <input type="number" placeholder="Quantity" value={quantity} onChange={(event)=>{setQuantity(event.target.value);}}/>
                 <input type="date" placeholder="Date" value={order_date} onChange={(event)=>{setOrder_date(event.target.value);}}/>
             </div>
-            <div className="submitButton" onClick={createNewOrder}>
+            <div className="submitButton" style={{marginBottom:"30px"}} onClick={createNewOrder}>
                 Add Order
             </div>
         </>
