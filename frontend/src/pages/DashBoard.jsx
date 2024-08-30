3a5376b54e8552fcd9a57d96dca074521f38bb1e
vishwajithBarad1 <vishwajithbarad@gmail.com>
2024-08-31 04:15:22 +0530

    added navBar component  and removed the repeated nav bar code

--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -3,11 +3,13 @@ import { useNavigate } from 'react-router-dom';
 import '../styles/dashboard.css'; // Importing a CSS file for styling
 import ProductDetails from "../components/ProductDetails"
 import axios from "axios";
+import NavBar from '../components/NavBar';
 function DashBoard() {
     const navigate = useNavigate();
     const [products,setProducts] = useState([]);
     const [totalStock,setTotalStock] = useState(0);
     const [totalBelowReorder,setTotalBelowReorder] = useState(0);
+    
     function totalStockValue(){
         const value = products.reduce((sum,product) => Number(sum) + Number(product.current_stock),0);
         let totalBelowReorder =  0
@@ -18,10 +20,6 @@ function DashBoard() {
         })
         setTotalStock(value);
         setTotalBelowReorder(totalBelowReorder)
-    }
-    function handleLogout(){
-        localStorage.removeItem("authToken");
-        navigate("/");
     }
     async function getProducts(){
         try{
@@ -48,18 +46,8 @@ function DashBoard() {
     return (
         <div>
             <div className="dashboard-container">
-            <div className="dashboard-buttons">
-                <button onClick={() => navigate('/dashboard')} className="dashboard-button">Dashboard</button>
-                <button onClick={() => navigate('/product')} className="dashboard-button">Products</button>
-                <button onClick={() => navigate('/suppliers')} className="dashboard-button">Suppliers</button>
-                <button onClick={() => navigate('/orders')} className="dashboard-button">Orders</button>
-                <button onClick={() => navigate('/reports')} className="dashboard-button">Reports</button>
-            </div>
-            <h1 className="dashboard-title">Inventory Management System</h1>
-            <div>
-                <button className='logout' onClick={handleLogout}>logout</button>
-            </div>
-            <div>
+            <NavBar page={"dashboard"}/>
+            <div style={{margin:"60px auto 10px auto"}}>
                 <span className='inventoryValues'>Total products : {products.length}</span>
                 <span className='inventoryValues'>Total stock value : {totalStock}</span>
                 <span className='inventoryValues'>Products Below Reorder Level : {totalBelowReorder}</span>
@@ -67,7 +55,6 @@ function DashBoard() {
             </div>
             <ProductDetails name={"name"} sku={"sku"} description={"description"} price={"price"} current_stock={"current_stock"} ></ProductDetails>
             {products.map((product) =>{return <ProductDetails name={product.name} sku={product.sku} description={product.description} price={product.price} current_stock={product.current_stock} stock={product.stock}/>})}
-            
         </div>
     );
 }
